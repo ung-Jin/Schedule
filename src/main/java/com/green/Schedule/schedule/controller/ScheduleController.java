246ed01a0c599f0343e7571a4afa28be214ce87e
rlawns2848-hue <rlawns2848@gmail.com>
2026-09-09 16:39:46 +0900

    관리자 일정관리 작업중

---
+++ src/main/java/com/green/Schedule/schedule/controller/ScheduleController.java
@@ -0,0 +1,24 @@
+package com.green.Schedule.schedule.controller;
+
+import com.green.Schedule.schedule.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.sql.Struct;
+
+@Controller
+@RequiredArgsConstructor
+public class ScheduleController {
+  private final ScheduleService scheduleService;
+
+  //관리자 AS 일정 관리 페이지
+  @GetMapping("as-schedule")
+  public String asSchedule(){
+
+    return "pages/schedule/admin_schedule";
+  }
+
+  //관리자 AS 일정 배정
+
+}
---
+++ src/main/java/com/green/Schedule/schedule/dto/ScheduleDTO.java
@@ -0,0 +1,7 @@
+package com.green.Schedule.schedule.dto;
+
+public class ScheduleDTO {
+
+
+
+}
